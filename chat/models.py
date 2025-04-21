36a1d88f68e914f193290550b3dcbd676e6d00d0
JuniorEyetemou <eyetemoujunior@gmail.com>
2025-04-21 22:12:52 +0200

    Nouvelle version avec base de donnée postgre de tchatApp

--- chat/models.py
+++ chat/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Room(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)  
 
     def __str__(self):
         return self.name
--- chat/urls.py
+++ chat/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', views.index, name="index"),
     path("<str:room_name>/<str:user_name>/", views.room, name="room"),
     path("upload/", views.upload_file, name='upload'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
